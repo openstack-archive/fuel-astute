392da172ed14daf0264f7b552cf3c0372b50b8b0
Vladimir Kozhukalov <kozhukalov@gmail.com>
2013-05-08 16:18:24 +0400

    Added cobbler api

---
+++ lib/astute/cobbler.rb
@@ -0,0 +1,277 @@
+require 'xmlrpc/client'
+
+module Astute
+  module Provision
+    class CobblerError < RuntimeError; end
+
+    class Cobbler
+
+      attr_reader :remote, :token
+
+      def initialize(o={})
+        Astute.logger.debug("Cobbler options: #{o.inspect}")
+
+        if /^http:\/\/([^:]+?):?(\d+)?(\/.+)/ =~ o['url']
+          host = $1
+          port = $2 || '80'
+          path = $3
+        else
+          host = o['host'] || 'localhost'
+          port = o['port'] || '80'
+          path = o['path'] || '/cobbler_api'
+        end
+        username = o['username'] || 'cobbler'
+        password = o['password'] || 'cobbler'
+
+        Astute.logger.debug("Connecting to cobbler with: host: #{host} port: #{port} path: #{path}")
+        @remote = XMLRPC::Client.new(host, path, port)
+        Astute.logger.debug("Trying to log in to cobbler with username: #{username}, password: #{password}")
+        @token = remote.call('login', username, password)
+      end
+
+      def item_from_hash(what, name, data, opts = {})
+        options = {
+          :item_preremove => true,
+        }.merge!(opts)
+        cobsh = Cobsh.new(data.merge({'what' => what, 'name' => name}))
+        cobblerized = cobsh.cobblerized
+
+        Astute.logger.debug("Creating/editing item from hash: #{cobsh.inspect}")
+        remove_item(what, name) if options[:item_preremove]
+        # get existent item id or create new one
+        item_id = get_item_id(what, name)
+
+        # defining all item options
+        cobblerized.each do |opt, value|
+          next if opt == 'interfaces'
+          Astute.logger.debug("Setting #{what} #{name} opt: #{opt}=#{value}")
+          remote.call('modify_item', what, item_id, opt, value, token)
+        end
+
+        # defining system interfaces
+        if what == 'system' and cobblerized.has_key?('interfaces')
+          Astute.logger.debug("Defining system interfaces #{name} #{cobblerized['interfaces']}")
+          remote.call('modify_system', item_id, 'modify_interface',
+                  cobblerized['interfaces'], token)
+        end
+
+        # save item into cobbler database
+        Astute.logger.debug("Saving #{what} #{name}")
+        remote.call('save_item', what, item_id, token)
+      end
+
+      def remove_item(what, name, recursive=true)
+        remote.call('remove_item', what, name, token, recursive) if item_exists(what, name)
+      end
+
+      def remove_system(name)
+        remove_item('system', name)
+      end
+
+      def item_exists(what, name)
+        remote.call('has_item', what, name)
+      end
+
+      def system_exists(name)
+        item_exists('system', name)
+      end
+
+      def get_item_id(what, name)
+        if item_exists(what, name)
+          item_id = remote.call('get_item_handle', what, name, token)
+        else
+          item_id = remote.call('new_item', what, token)
+          remote.call('modify_item', what, item_id, 'name', name, token)
+        end
+        item_id
+      end
+
+      def sync
+        remote.call('sync', token)
+      end
+
+      def power(name, power)
+        options = {"systems" => [name], "power" => power}
+        remote.call('background_power_system', options, token)
+      end
+
+      def power_on(name)
+        power(name, 'on')
+      end
+
+      def power_off(name)
+        power(name, 'off')
+      end
+
+      def power_reboot(name)
+        power(name, 'reboot')
+      end
+
+    end
+
+    class Cobsh < ::Hash
+      ALIASES = {
+        'ks_meta' => ['ksmeta'],
+        'mac_address' => ['mac'],
+        'ip_address' => ['ip'],
+      }
+
+      # these fields can be get from the cobbler code
+      # you can just import cobbler.item_distro.FIELDS
+      # or cobbler.item_system.FIELDS
+      FIELDS = {
+        'system' => {
+          'fields' => [
+            'name', 'owners', 'profile', 'image', 'status', 'kernel_options',
+            'kernel_options_post', 'ks_meta', 'enable_gpxe', 'proxy',
+            'netboot_enabled', 'kickstart', 'comment', 'server',
+            'virt_path', 'virt_type', 'virt_cpus', 'virt_file_size',
+            'virt_disk_driver', 'virt_ram', 'virt_auto_boot', 'power_type',
+            'power_address', 'power_user', 'power_pass', 'power_id',
+            'hostname', 'gateway', 'name_servers', 'name_servers_search',
+            'ipv6_default_device', 'ipv6_autoconfiguration', 'mgmt_classes',
+            'mgmt_parameters', 'boot_files', 'fetchable_files',
+            'template_files', 'redhat_management_key', 'redhat_management_server',
+            'repos_enabled', 'ldap_enabled', 'ldap_type', 'monit_enabled',
+          ],
+          'interfaces_fields' => [
+            'mac_address', 'mtu', 'ip_address', 'interface_type',
+            'interface_master', 'bonding_opts', 'bridge_opts',
+            'management', 'static', 'netmask', 'dhcp_tag', 'dns_name',
+            'static_routes', 'virt_bridge', 'ipv6_address', 'ipv6_secondaries',
+            'ipv6_mtu', 'ipv6_static_routes', 'ipv6_default_gateway'
+          ],
+          'special' => ['interfaces', 'interfaces_extra']
+        },
+        'profile' => {
+          'fields' => [
+            'name', 'owners', 'distro', 'parent', 'enable_gpxe',
+            'enable_menu', 'kickstart', 'kernel_options', 'kernel_options_post',
+            'ks_meta', 'proxy', 'repos', 'comment', 'virt_auto_boot',
+            'virt_cpus', 'virt_file_size', 'virt_disk_driver',
+            'virt_ram', 'virt_type', 'virt_path', 'virt_bridge',
+            'dhcp_tag', 'server', 'name_servers', 'name_servers_search',
+            'mgmt_classes', 'mgmt_parameters', 'boot_files', 'fetchable_files',
+            'template_files', 'redhat_management_key', 'redhat_management_server'
+          ]
+        },
+        'distro' => {
+          'fields' => ['name', 'owners', 'kernel', 'initrd', 'kernel_options',
+            'kernel_options_post', 'ks_meta', 'arch', 'breed',
+            'os_version', 'comment', 'mgmt_classes', 'boot_files',
+            'fetchable_files', 'template_files', 'redhat_management_key',
+            'redhat_management_server']
+        }
+
+      }
+
+      def initialize(h)
+        Astute.logger.debug("Cobsh is initialized with: #{h.inspect}")
+        raise CobblerError, "Cobbler hash must have 'name' key" unless h.has_key? 'name'
+        raise CobblerError, "Cobbler hash must have 'what' key" unless h.has_key? 'what'
+        raise CobblerError, "Unsupported 'what' value" unless FIELDS.has_key? h['what']
+        h.each{|k, v| store(k, v)}
+      end
+
+
+      def cobblerized
+        Astute.logger.debug("Cobblerizing hash: #{inspect}")
+        ch = {}
+        ks_meta = ""
+
+        each do |k, v|
+          k = aliased(k)
+          raise CobblerError, "Wrong cobbler data: #{k} is duplicated" if ch.has_key?(k)
+
+          # skiping not valid item options
+          unless valid_field?(k)
+            Astute.logger.debug("Key #{k} is not valid. Will be skipped.")
+            next
+          end
+
+          # here we don't store ks_meta directly in ch (cobblerized hash)
+          # instead we just append ks_meta value to store it later
+          if k == 'ks_meta'
+            if v.kind_of?(Hash)
+              v.each do |ks_meta_key, ks_meta_value|
+                ks_meta << " #{ks_meta_key}=#{ks_meta_value}"
+              end
+            elsif v.kind_of?(String)
+              ks_meta << " #{v}"
+            else
+              raise "Wrong ks_meta format. It must be Hash or String"
+            end
+          end
+
+          # special handling for system interface fields
+          # which are the only objects in cobbler that will ever work this way
+          if k == 'interfaces'
+            ch.store('interfaces', cobblerized_interfaces)
+            next
+          end
+
+          # here we convert interfaces_extra options into ks_meta format
+          if k == 'interfaces_extra'
+            ks_meta << cobblerized_interfaces_extra
+            next
+          end
+
+          ch.store(k, v)
+        end # keys.each do |k|
+        ch.store('ks_meta', ks_meta.strip) if ks_meta.strip.length > 0
+        ch
+      end
+
+      def aliased(k)
+        # converting 'foo-bar' keys into 'foo_bar' keys
+        k1 = k.gsub(/-/,'_')
+        # converting orig keys into alias keys
+        # example: 'ksmeta' into 'ks_meta'
+        k2 = (ALIASES.each_key.select{|ak| ALIASES[ak].include?(k1)}[0] or k1)
+        Astute.logger.debug("Key #{k} aliased with #{k2}") if k != k2
+        k2
+      end
+
+      def valid_field?(k)
+        (FIELDS[fetch('what')]['fields'].include?(k) or
+          (FIELDS[fetch('what')]['special'] or []).include?(k))
+      end
+
+      def valid_interface_field?(k)
+        (FIELDS[fetch('what')]['interfaces_fields'] or []).include?(k)
+      end
+
+      def cobblerized_interfaces
+        interfaces = {}
+        fetch('interfaces').each do |iname, ihash|
+          ihash.each do |iopt, ivalue|
+            iopt = aliased(iopt)
+            if interfaces.has_key?("#{iopt}-#{iname}")
+              raise CobblerError, "Wrong interface cobbler data: #{iopt} is duplicated"
+            end
+            unless valid_interface_field?(iopt)
+              Astute.logger.debug("Interface key #{iopt} is not valid. Skipping")
+              next
+            end
+            Astute.logger.debug("Defining interfaces[#{iopt}-#{iname}] = #{ivalue}")
+            interfaces["#{iopt}-#{iname}"] = ivalue
+          end
+        end
+        interfaces
+      end
+
+      def cobblerized_interfaces_extra
+        # here we just want to convert interfaces_extra into ks_meta
+        interfaces_extra_str = ""
+        fetch('interfaces_extra').each do |iname, iextra|
+          iextra.each do |k, v|
+            Astute.logger.debug("Adding into ks_meta interface_extra_#{iname}_#{k}=#{v}")
+            interfaces_extra_str << " interface_extra_#{iname}_#{k}=#{v}"
+          end
+        end
+        interfaces_extra_str
+      end
+    end
+
+  end
+end
---
+++ spec/unit/cobbler_spec.rb
@@ -0,0 +1,367 @@
+#!/usr/bin/env rspec
+
+require File.join(File.dirname(__FILE__), "..", "spec_helper")
+include Astute::Provision
+
+describe Cobbler do
+  include SpecHelpers
+
+
+
+  it "should be able to be initialized with 'url'" do
+    host = "host.domain.tld"
+    port = "1234"
+    path = "/api"
+    username = 'user'
+    password = 'pass'
+
+    remote = mock() do
+      expects(:call).with('login', username, password)
+    end
+    tmp = XMLRPC::Client
+    XMLRPC::Client = mock() do
+      expects(:new).with(host, path, port).returns(remote)
+    end
+    Astute::Provision::Cobbler.new(
+                               'url' => "http://#{host}:#{port}#{path}",
+                               'username' => username,
+                               'password' => password
+                               )
+    XMLRPC::Client = tmp
+  end
+
+  it "should be able to be initialized with 'host', 'port', 'path'" do
+    host = "host.domain.tld"
+    port = "1234"
+    path = "/api"
+    username = 'user'
+    password = 'pass'
+
+    remote = mock() do
+      expects(:call).with('login', username, password)
+    end
+    tmp = XMLRPC::Client
+    XMLRPC::Client = mock() do
+      expects(:new).with(host, path, port).returns(remote)
+    end
+    Astute::Provision::Cobbler.new(
+                               'host' => host,
+                               'port' => port,
+                               'path' => path,
+                               'username' => username,
+                               'password' => password
+                               )
+    XMLRPC::Client = tmp
+  end
+
+  context "cobbler methods" do
+    before(:each) do
+      remote = mock() do
+        stubs(:call)
+        stubs(:call).with('login', 'cobbler', 'cobbler').returns('remotetoken')
+      end
+      @tmp = XMLRPC::Client
+      XMLRPC::Client = mock() do
+        stubs(:new).returns(remote)
+      end
+
+      @data = {
+        'profile' => 'centos63-x86_64',
+        'power_type' => 'ssh',
+        'power_user' => 'root',
+        'power_pass' => '/root/.ssh/bootstrap.rsa',
+        'power-address' => '1.2.3.5',
+        'hostname' => 'name.domain.tld',
+        'name_servers' => '1.2.3.4 1.2.3.100',
+        'name_servers_search' => 'some.domain.tld domain.tld',
+        'netboot_enabled' => '1',
+        'ks_meta' => 'some_param=1 another_param=2',
+        'interfaces' => {
+          'eth0' => {
+            'mac_address' => '00:00:00:00:00:00',
+            'static' => '1',
+            'netmask' => '255.255.255.0',
+            'ip_address' => '1.2.3.5',
+            'dns_name' => 'node.mirantis.net',
+          },
+          'eth1' => {
+            'mac_address' => '00:00:00:00:00:01',
+            'static' => '0',
+            'netmask' => '255.255.255.0',
+            'ip_address' => '1.2.3.6',
+          }
+        },
+        'interfaces_extra' => {
+          'eth0' => {
+            'peerdns' => 'no',
+            'onboot' => 'yes',
+          },
+          'eth1' => {
+            'peerdns' => 'no',
+            'onboot' => 'yes',
+          }
+        }
+      }
+    end
+
+    after(:each) do
+      XMLRPC::Client = @tmp
+    end
+
+    it "item_from_hash should remove item if 'item_preremove' is true" do
+      Astute::Provision::Cobbler.any_instance do
+        expects(:remove_item).with('system', 'name')
+      end
+      cobbler = Astute::Provision::Cobbler.new
+      cobbler.item_from_hash('system', 'name', {}, :item_preremove => true)
+    end
+
+    it "item_from_hash should create new item if it does not exist" do
+      cobbler = Astute::Provision::Cobbler.new
+      cobbler.remote.expects(:call).with('has_item', 'system', 'name').returns(false)
+      cobbler.remote.expects(:call).with('new_item', 'system', cobbler.token).returns('itemid')
+      cobbler.remote.expects(:call).with('modify_item', 'system', 'itemid', 'name', 'name', cobbler.token)
+      cobbler.item_from_hash('system', 'name', {}, :item_preremove => true)
+    end
+
+    it "item_from_hash should modify existent item if it does exist" do
+      cobbler = Astute::Provision::Cobbler.new
+      cobbler.remote.expects(:call).with('has_item', 'system', 'name').returns(true)
+      cobbler.remote.expects(:call).with('get_item_handle', 'system', 'name', cobbler.token).returns('itemid')
+      cobbler.item_from_hash('system', 'name', {}, :item_preremove => false)
+    end
+
+    it "item_from_hash should modify item with cobblerized data" do
+      cobblerized_data = Astute::Provision::Cobsh.new(@data.merge({'what' => 'system', 'name' => 'name'})).cobblerized
+      cobbler = Astute::Provision::Cobbler.new
+      cobbler.stubs(:get_item_id).with('system', 'name').returns('itemid')
+      cobblerized_data.each do |opt, value|
+        next if opt == 'interfaces'
+        cobbler.remote.expects(:call).with(
+                                      'modify_item',
+                                      'system',
+                                      'itemid',
+                                      opt,
+                                      value,
+                                      'remotetoken'
+                                      )
+      end
+      cobbler.item_from_hash('system', 'name', @data, :item_preremove => true)
+    end
+
+    it "item_from_hash should modify 'system' interfaces with cobblerized['interfaces']" do
+      cobblerized_data = Astute::Provision::Cobsh.new(@data.merge({'what' => 'system', 'name' => 'name'})).cobblerized
+      cobbler = Astute::Provision::Cobbler.new
+      cobbler.stubs(:get_item_id).with('system', 'name').returns('itemid')
+      cobbler.remote.expects(:call).with(
+                                    'modify_system',
+                                    'itemid',
+                                    'modify_interface',
+                                    cobblerized_data['interfaces'],
+                                    'remotetoken')
+      cobbler.item_from_hash('system', 'name', @data, :item_preremove => true)
+    end
+
+  end
+end
+
+
+
+describe Cobsh do
+  before(:each) do
+
+    @aliases = {
+      'ks_meta' => ['ksmeta'],
+      'mac_address' => ['mac'],
+      'ip_address' => ['ip'],
+    }
+
+    @fields = {
+      'system' => {
+        'fields' => [
+          'name', 'owners', 'profile', 'image', 'status', 'kernel_options',
+          'kernel_options_post', 'ks_meta', 'enable_gpxe', 'proxy',
+          'netboot_enabled', 'kickstart', 'comment', 'server',
+          'virt_path', 'virt_type', 'virt_cpus', 'virt_file_size',
+          'virt_disk_driver', 'virt_ram', 'virt_auto_boot', 'power_type',
+          'power_address', 'power_user', 'power_pass', 'power_id',
+          'hostname', 'gateway', 'name_servers', 'name_servers_search',
+          'ipv6_default_device', 'ipv6_autoconfiguration', 'mgmt_classes',
+          'mgmt_parameters', 'boot_files', 'fetchable_files',
+          'template_files', 'redhat_management_key', 'redhat_management_server',
+          'repos_enabled', 'ldap_enabled', 'ldap_type', 'monit_enabled',
+        ],
+        'interfaces_fields' => [
+          'mac_address', 'mtu', 'ip_address', 'interface_type',
+          'interface_master', 'bonding_opts', 'bridge_opts',
+          'management', 'static', 'netmask', 'dhcp_tag', 'dns_name',
+          'static_routes', 'virt_bridge', 'ipv6_address', 'ipv6_secondaries',
+          'ipv6_mtu', 'ipv6_static_routes', 'ipv6_default_gateway'
+        ],
+        'special' => ['interfaces', 'interfaces_extra']
+      },
+      'profile' => {
+        'fields' => [
+          'name', 'owners', 'distro', 'parent', 'enable_gpxe',
+          'enable_menu', 'kickstart', 'kernel_options', 'kernel_options_post',
+          'ks_meta', 'proxy', 'repos', 'comment', 'virt_auto_boot',
+          'virt_cpus', 'virt_file_size', 'virt_disk_driver',
+          'virt_ram', 'virt_type', 'virt_path', 'virt_bridge',
+          'dhcp_tag', 'server', 'name_servers', 'name_servers_search',
+          'mgmt_classes', 'mgmt_parameters', 'boot_files', 'fetchable_files',
+          'template_files', 'redhat_management_key', 'redhat_management_server'
+        ]
+      },
+      'distro' => {
+        'fields' => ['name', 'owners', 'kernel', 'initrd', 'kernel_options',
+          'kernel_options_post', 'ks_meta', 'arch', 'breed',
+          'os_version', 'comment', 'mgmt_classes', 'boot_files',
+          'fetchable_files', 'template_files', 'redhat_management_key',
+          'redhat_management_server']
+      }
+    }
+
+    @minimal_data = {
+      'what' => 'system',
+      'name' => 'name',
+    }
+
+  end
+
+  it "should raise exception when init data do not contain 'what'" do
+    expect {
+      Astute::Provision::Cobsh.new({'what'=>'system'})
+    }.to raise_exception(/Cobbler hash must have 'name' key/)
+  end
+
+  it "should raise exception when init data do not contain 'name'" do
+    expect {
+      Astute::Provision::Cobsh.new({'name'=>'name'})
+    }.to raise_exception(/Cobbler hash must have 'what' key/)
+  end
+
+  it "should raise exception when init data 'what' is not supported" do
+    expect {
+      Astute::Provision::Cobsh.new({
+                                     'what' => 'unsupported',
+                                     'name'=>'name'
+                                   })
+    }.to raise_exception(/Unsupported 'what' value/)
+  end
+
+  it "should replace '-' into '_' in init data keys" do
+    data = @minimal_data.merge({'power_id' => 'blabla'})
+    expected = {
+      'name' => data['name'],
+      'power_id' => 'blabla'
+    }
+    Astute::Provision::Cobsh.new(data).cobblerized.should eql(expected)
+  end
+
+  it "returns cobblerizied data with aliased keys" do
+    @fields.each do |what, what_data|
+      @aliases.each do |main_alias, alias_list|
+        data = {'name' => 'name'}
+        expected = {'name' => 'name'}
+        if what_data['fields'].include? main_alias
+          data['what'] = what
+          alias_list.each do |a|
+            data2 = data.merge({a => 'blabla'})
+            expected2 = expected.merge({main_alias => 'blabla'})
+            Astute::Provision::Cobsh.new(data2).cobblerized.should eql(expected2)
+          end
+        elsif what_data.has_key? 'interfaces_fields' and what_data['interfaces_fields'].include? main_alias
+          data['what'] = what
+          alias_list.each do |a|
+            data2 = data.merge({
+                                 'interfaces' => {
+                                   'eth0' => {a => 'blabla'}
+                                 }
+                               })
+            expected2 = expected.merge({
+                                         'interfaces' => {
+                                           "#{main_alias}-eth0" => 'blabla'
+                                         }
+                                       })
+            Astute::Provision::Cobsh.new(data2).cobblerized.should eql(expected2)
+          end
+        end
+      end
+    end
+  end
+
+  it "should raise exception when keys are duplicated in init data" do
+    data = @minimal_data.merge({'ks-meta' => 'ks-meta', 'ksmeta' => 'ksmeta'})
+    expect {
+      Astute::Provision::Cobsh.new(data).cobblerized
+    }.to raise_exception(/Wrong cobbler data: .* is duplicated/)
+  end
+
+  it "should cut out 'system' unsupported keys" do
+    data = {'what' => 'system'}
+    expected = {}
+    @fields['system']['fields'].each do |f|
+      data[f] = f
+      expected[f] = f
+    end
+    @fields['system']['interfaces_fields'].each do |f|
+      data['interfaces'] = {'eth0' => {}} unless data.has_key? 'interfaces'
+      data['interfaces']['eth0'][f] = f
+      expected['interfaces'] = {} unless expected.has_key? 'interfaces'
+      expected['interfaces']["#{f}-eth0"] = f
+    end
+    data['unsupported'] = 'unsupported'
+    Astute::Provision::Cobsh.new(data).cobblerized.should eql(expected)
+  end
+
+  it "should cut out 'profile' unsupported keys" do
+    data = {'what' => 'profile'}
+    expected = {}
+    @fields['profile']['fields'].each do |f|
+      data[f] = f
+      expected[f] = f
+    end
+    data['unsupported'] = 'unsupported'
+    Astute::Provision::Cobsh.new(data).cobblerized.should eql(expected)
+  end
+
+  it "should cut out 'distro' unsupported keys" do
+    data = {'what' => 'distro'}
+    expected = {}
+    @fields['distro']['fields'].each do |f|
+      data[f] = f
+      expected[f] = f
+    end
+    data['unsupported'] = 'unsupported'
+    Astute::Provision::Cobsh.new(data).cobblerized.should eql(expected)
+  end
+
+  it "should append extra interfaces data into ks_meta" do
+    RSpec::Matchers.define :ks_meta_equal? do |ks_meta|
+      match do |ks_meta_self|
+        equal = true
+        ks_meta_self.strip.split.each do |i|
+          equal = false unless ks_meta =~ /.*?#{i}.*?/
+        end
+        ks_meta.strip.split.each do |i|
+          equal = false unless ks_meta_self =~ /.*?#{i}.*?/
+        end
+        equal
+      end
+    end
+
+    data = @minimal_data.merge({
+                                 'interfaces_extra' => {
+                                   'eth0' => {
+                                     'blabla' => 'blabla',
+                                     'blabla2' => 'blabla2',
+                                   }
+                                 }
+                               })
+    expected_ks_meta = "interface_extra_eth0_blabla=blabla interface_extra_eth0_blabla2=blabla2"
+    Astute::Provision::Cobsh.new(data).cobblerized['ks_meta'].should ks_meta_equal?(expected_ks_meta)
+  end
+
+end
+
+
