030dd02e4ee3411bb7a49e2d8b101bdc7d80cab2
Dmitry Pyzhov <dpyzhov@mirantis.com>
2013-06-04 21:15:26 +0400

    Storage network on controllers

--- lib/astute/deployment_engine.rb
+++ lib/astute/deployment_engine.rb
@@ -67,6 +67,7 @@ module Astute
       ctrl_nodes = attrs['controller_nodes']
       ctrl_manag_addrs = {}
       ctrl_public_addrs = {}
+      ctrl_storage_addrs = {}
       ctrl_nodes.each do |n|
         # current puppet modules require `hostname -s`
         hostname = n['fqdn'].split(/\./)[0]
@@ -74,15 +75,18 @@ module Astute
                    n['network_data'].select {|nd| nd['name'] == 'management'}[0]['ip'].split(/\//)[0]})
         ctrl_public_addrs.merge!({hostname =>
                    n['network_data'].select {|nd| nd['name'] == 'public'}[0]['ip'].split(/\//)[0]})
+        ctrl_storage_addrs.merge!({hostname =>
+                   n['network_data'].select {|nd| nd['name'] == 'storage'}[0]['ip'].split(/\//)[0]})
       end
 
       attrs['nodes'] = ctrl_nodes.map do |n|
         {
-          'name'             => n['fqdn'].split(/\./)[0],
-          'role'             => 'controller',
-          'internal_address' => n['network_data'].select {|nd| nd['name'] == 'management'}[0]['ip'].split(/\//)[0],
-          'public_address'   => n['network_data'].select {|nd| nd['name'] == 'public'}[0]['ip'].split(/\//)[0],
-          'mountpoints'      => "1 1\n2 2",
+          'name'                 => n['fqdn'].split(/\./)[0],
+          'role'                 => 'controller',
+          'internal_address'     => n['network_data'].select {|nd| nd['name'] == 'management'}[0]['ip'].split(/\//)[0],
+          'public_address'       => n['network_data'].select {|nd| nd['name'] == 'public'}[0]['ip'].split(/\//)[0],
+          'mountpoints'          => "1 1\n2 2",
+          'zone'                 => n['id'],
           'storage_local_net_ip' => n['network_data'].select {|nd| nd['name'] == 'storage'}[0]['ip'].split(/\//)[0],
         }
       end
@@ -91,6 +95,7 @@ module Astute
       attrs['master_hostname'] = ctrl_nodes[0]['fqdn'].split(/\./)[0]
       attrs['ctrl_public_addresses'] = ctrl_public_addrs
       attrs['ctrl_management_addresses'] = ctrl_manag_addrs
+      attrs['ctrl_storage_addresses'] = ctrl_storage_addrs
       attrs
     end
 
