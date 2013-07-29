0fba00e5ee2dc7636c11946d42cd426c35d5ce42
Evgeniy L <test0x0000@gmail.com>
2013-07-29 17:44:02 +0400

    [RedhatChecket] DRY in specs

--- spec/unit/redhat_checker_spec.rb
+++ spec/unit/redhat_checker_spec.rb
@@ -21,7 +21,7 @@ require File.join(File.dirname(__FILE__), '../spec_helper')
 describe Astute::RedhatChecker do
   include SpecHelpers
 
-  let(:data) do
+  let(:redhat_credentials) do
     {
       'username' => 'user',
       'password' => 'password'
@@ -30,19 +30,24 @@ describe Astute::RedhatChecker do
 
   let(:reporter) { mock('reporter') }
   let(:ctx) { Astute::Context.new('task-uuuid', reporter) }
-  let(:redhat_checker) { described_class.new(ctx, data) }
+  let(:redhat_checker) { described_class.new(ctx, redhat_credentials) }
 
   let!(:rpcclient) { mock_rpcclient }
+  let(:success_result) { {'status' => 'ready', 'progress' => 100} }
 
   def execute_returns(data)
     result = mock_mc_result({:data => data })
     rpcclient.expects(:execute).once.returns([result])
   end
 
+  def should_report_once(data)
+    reporter.expects(:report).once.with(data)
+  end
+
   describe '#check_redhat_credentials' do
     it 'should report ready if exit_code 0' do
       execute_returns({:exit_code => 0})
-      reporter.expects(:report).once.with({'status' => 'ready', 'progress' => 100})
+      should_report_once(success_result)
 
       redhat_checker.check_redhat_credentials
     end
@@ -51,7 +56,7 @@ describe Astute::RedhatChecker do
   describe '#check_redhat_licenses' do
     it 'should report ready if exit_code 0' do
       execute_returns({:exit_code => 0})
-      reporter.expects(:report).once.with({'status' => 'ready', 'progress' => 100})
+      should_report_once(success_result)
 
       nodes = []
       redhat_checker.check_redhat_licenses(nodes)
@@ -61,7 +66,7 @@ describe Astute::RedhatChecker do
   describe '#redhat_has_at_least_one_license' do
     it 'should report ready if exit_code 0' do
       execute_returns({:exit_code => 0})
-      reporter.expects(:report).once.with({'status' => 'ready', 'progress' => 100})
+      should_report_once(success_result)
 
       redhat_checker.redhat_has_at_least_one_license
     end
