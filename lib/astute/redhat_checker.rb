6ea1ee989f1725d7f7c92a2a834e2061f65c2b70
Evgeniy L <test0x0000@gmail.com>
2013-07-29 17:43:32 +0400

    [RedhatChecker] add labels for result string

--- lib/astute/redhat_checker.rb
+++ lib/astute/redhat_checker.rb
@@ -73,7 +73,7 @@ module Astute
       if exit_code == 0
         report_success
       else
-        error = errors[exit_code] || "#{result[:stdout]} #{result[:stderr]}"
+        error = errors[exit_code] || "Stdout: #{result[:stdout]} Stderr: #{result[:stderr]}"
         report_error(error)
       end
     end
