21c2c4620e19fdd50b2fdedb701798fbae3852dd
Vladmir Sharhsov(warpc) <vsharshov@gmail.com>
2013-08-14 10:50:07 +0400

    Add additional checks for YAML deploy config (quantum)

--- lib/astute/cli/yaml_validator.rb
+++ lib/astute/cli/yaml_validator.rb
@@ -14,6 +14,9 @@
 
 require 'kwalify'
 
+CIDR_REGEXP = '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|
+              2[0-4][0-9]|25[0-5])(\/(\d|[1-2]\d|3[0-2]))$'
+
 module Astute
   module Cli
     class YamlValidator < Kwalify::Validator
@@ -39,19 +42,43 @@ module Astute
         super(@schema)
       end
     
-    # hook method called by Validator#validate()
+      # hook method called by Validator#validate()
       def validate_hook(value, rule, path, errors)
-        # case rule.name
-  #       when 'use_for_provision'
-  #         if value['name'] == 'bad'
-  #           reason = value['reason']
-  #           if !reason || reason.empty?
-  #             msg = "reason is required when answer is 'bad'."
-  #             errors << Kwalify::ValidationError.new(msg, path)
-  #           end
-  #         end
-  #       end
+        case rule.name
+        when 'Attributes'
+          require_field(value, path, errors, 'quantum', true, 'quantum_parameters')
+          require_field(value, path, errors, 'quantum', true, 'fixed_network_range')
+          require_field(value, path, errors, 'quantum', true, 'quantum_access')
+          #require_field(value, path, errors, 'quantum', false, 'floating_network_range')
+          floating_network_range = value['floating_network_range']
+          quantum = value['quantum']
+          if quantum
+            cidr = Regexp.new(CIDR_REGEXP)
+            if cidr.match(floating_network_range).nil?
+              msg = "'floating_network_range' is required CIDR notation when quantum is 'true'"
+              errors << Kwalify::ValidationError.new(msg, path)
+            end
+          elsif !floating_network_range.is_a?(Array)
+            msg = "'floating_network_range' is required array of IPs when quantum is 'false'"
+            errors << Kwalify::ValidationError.new(msg, path)
+          end
+        when 'Nodes'
+          #require_field(value, path, errors, 'quantum', true, 'public_br')
+          #require_field(value, path, errors, 'quantum', true, 'internal_br')
+        end
       end
-    end
-  end
-end
+      
+      private
+        
+      def require_field(value, path, errors, condition_key, condition_value, key)
+        return if value[condition_key] != condition_value
+        field_value = value[key]
+        if field_value.nil? || field_value.empty?
+          msg = "#{key} is required when #{condition_key} is '#{condition_value}'"
+          errors << Kwalify::ValidationError.new(msg, path)
+        end
+      end
+      
+    end # YamlValidator
+  end # Cli
+end # Astute
