e0e988d598b7cb312a5f6889887347ec717769c0
Evgeniy L <eli@mirantis.com>
2013-09-25 16:33:25 +0400

    Removed nodes filtering

--- lib/astute/deployment_engine/nailyfact.rb
+++ lib/astute/deployment_engine/nailyfact.rb
@@ -15,7 +15,7 @@
 require 'json'
 
 class Astute::DeploymentEngine::NailyFact < Astute::DeploymentEngine
-  
+
   # Just merge attributes of concrete node
   # with attributes of cluster
   def create_facts(node_attrs)
@@ -31,45 +31,27 @@ class Astute::DeploymentEngine::NailyFact < Astute::DeploymentEngine
   def deploy_piece(nodes, retries=2, change_node_status=true)
     return false unless validate_nodes(nodes)
 
-    nodes_to_deploy = get_nodes_to_deploy(nodes)
-    if nodes_to_deploy.empty?
+    if nodes.empty?
       Astute.logger.info "#{@ctx.task_id}: Returning from deployment stage. No nodes to deploy"
       return
     end
 
     Astute.logger.info "#{@ctx.task_id}: Calculation of required attributes to pass, include netw.settings"
-    @ctx.reporter.report(nodes_status(nodes_to_deploy, 'deploying', {'progress' => 0}))
+    @ctx.reporter.report(nodes_status(nodes, 'deploying', {'progress' => 0}))
 
     begin
-      @ctx.deploy_log_parser.prepare(nodes_to_deploy)
+      @ctx.deploy_log_parser.prepare(nodes)
     rescue => e
       Astute.logger.warn "Some error occurred when prepare LogParser: #{e.message}, trace: #{e.format_backtrace}"
     end
 
-    nodes_to_deploy.each do |node|
+    nodes.each do |node|
       Astute::Metadata.publish_facts @ctx, node['uid'], create_facts(node)
     end
     Astute.logger.info "#{@ctx.task_id}: Required attrs/metadata passed via facts extension. Starting deployment."
 
-    Astute::PuppetdDeployer.deploy(@ctx, nodes_to_deploy, retries, change_node_status)
-    
-    nodes_roles = nodes_to_deploy.map { |n| {n['uid'] => n['role']} }
+    Astute::PuppetdDeployer.deploy(@ctx, nodes, retries, change_node_status)
+    nodes_roles = nodes.map { |n| {n['uid'] => n['role']} }
     Astute.logger.info "#{@ctx.task_id}: Finished deployment of nodes => roles: #{nodes_roles.inspect}"
   end
-
-  private
-  def get_nodes_to_deploy(nodes)
-    Astute.logger.info "#{@ctx.task_id}: Getting which nodes to deploy"
-    nodes_to_deploy = []
-    nodes.each do |node|
-      if node['status'] != 'ready'
-        nodes_to_deploy << node
-      else
-        Astute.logger.info "#{@ctx.task_id}: Not adding node #{node['uid']} with hostname #{node['name']} as it does not require deploying."
-      end
-    end
-
-    nodes_to_deploy
-  end
-
 end
