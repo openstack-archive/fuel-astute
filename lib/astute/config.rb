7b77437a5d87dd5d06b45d152df80a681ffa25eb
Vladimir Kuklin <vkuklin@mirantis.com>
2013-12-11 13:00:12 +0400

    Rename ssh_keys for quantum to neutron in astute
    Change-Id: Iae4d8b6f8c2926a2b0a7fbb2b7960e4a4f6adc08
    Closes-bug: 1259604

--- lib/astute/config.rb
+++ lib/astute/config.rb
@@ -65,7 +65,7 @@ module Astute
     conf[:REDHAT_CHECK_CREDENTIALS_TIMEOUT] = 30  # checking redhat credentials througs mcollective
     conf[:REDHAT_GET_LICENSES_POOL_TIMEOUT] = 60  # getting redhat licenses through mcollective
 
-    conf[:PUPPET_SSH_KEYS] = ['quantum', 'nova', 'ceph', 'mysql']  # name of ssh keys what will be generated
+    conf[:PUPPET_SSH_KEYS] = ['neutron', 'nova', 'ceph', 'mysql']  # name of ssh keys what will be generated
                                                         #and uploaded to all nodes before deploy
     conf[:MAX_NODES_PER_CALL] = 50        # how many nodes to deploy in one puppet call
 
