c71e087a3bb0f4afbe8685e6614f637630f0c055
Vladimir <vsharshov@mirantis.com>
2013-12-18 14:18:49 +0400

    Increase puppet deploy timeout
      -Scenarios became more complicated and require more time to deploy
    
    Change-Id: Ic297c97dcbe54b14956dfea186189aad001d06a0
    Related-Bug: #1248744

--- lib/astute/config.rb
+++ lib/astute/config.rb
@@ -53,7 +53,7 @@ module Astute
 
   def self.default_config
     conf = {}
-    conf[:PUPPET_TIMEOUT] = 60 * 60       # maximum time it waits for the whole deployment
+    conf[:PUPPET_TIMEOUT] = 90 * 60       # maximum time it waits for the whole deployment
     conf[:PUPPET_DEPLOY_INTERVAL] = 2     # sleep for ## sec, then check puppet status again
     conf[:PUPPET_FADE_TIMEOUT] = 120      # how long it can take for puppet to exit after dumping to last_run_summary
     conf[:MC_RETRIES] = 5                 # MClient tries to call mcagent before failure
