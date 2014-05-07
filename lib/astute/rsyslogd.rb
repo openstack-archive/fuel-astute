87594cf7953b465b61a8a4c25ee8e764a3160d3d
Dima Shulyak <dshulyak@mirantis.com>
2014-05-07 15:35:35 +0300

    WIP: Send sighup to rsyslogd
    After remove_nodes action, create mcollecte client
    with execute_shell_command plugin for master node, and perform:
    
    ssh root@#{master_ip} 'for pids in $(pidof rsyslogd); do echo kill -HUP $pids; done'
    
    Change-Id: I8ab764aaa3bceb9c08c99d5bfe08582c83e267c7
    Closes-Bug: #1315382

---
+++ lib/astute/rsyslogd.rb
@@ -0,0 +1,39 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+module Astute
+  class Rsyslogd
+
+    def send_sighup(ctx, master_ip)
+        shell = MClient.new(ctx, 'execute_shell_command', ['master'],
+                            check_result=true, timeout=5, retries=1)
+        cmd = "ssh root@#{master_ip} 'for pids in $(pidof rsyslogd); do echo kill -HUP $pids; done'"
+
+        begin
+            result = shell.execute(:cmd => cmd).first.results
+
+            Astute.logger.info("#{ctx.task_id}: \
+    stdout: #{result[:data][:stdout]} stderr: #{result[:data][:stderr]} \
+    exit code: #{result[:data][:exit_code]}")
+        rescue Timeout::Error
+            msg = "Sending SIGHUP to rsyslogd is timed out."
+            Astute.logger.warning("#{ctx.task_id}: #{msg}")
+        rescue => e
+            msg = "Exception occured during sending SIGHUP to rsyslogd, message: #{e.message} \
+    trace: #{e.backtrace.inspect}"
+            Astute.logger.error("#{ctx.task_id}: #{msg}")
+        end
+    end
+  end
+end
