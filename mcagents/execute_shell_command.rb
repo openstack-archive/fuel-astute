8073120b0e922b13023be4c362956a6699693099
Evgeniy L <eli@mirantis.com>
2014-05-23 12:30:45 +0000

    Revert "Use recommended method to execute shell commands"
    This reverts commit b110b31dcbcdbadf8eae0e0e4d526cfa0a7e4f19.
    
    Change-Id: I6ac699675436588738ef251b3a07b815b55cbd44

--- mcagents/execute_shell_command.rb
+++ mcagents/execute_shell_command.rb
@@ -20,11 +20,36 @@ module MCollective
     class Execute_shell_command < RPC::Agent
 
       action 'execute' do
-        reply[:exit_code], reply[:stdout], reply[:stderr] = run(request[:cmd],
-                                                                :stdout => :stdout,
-                                                                :stderr => :stderr)
-        reply[:stdout] ||= ""
-        reply[:stderr] ||= ""
+        reply[:stdout], reply[:stderr], reply[:exit_code] = runcommand(request[:cmd])
+      end
+
+      private
+      def runcommand(cmd)
+        # We cannot use Shell from puppet because
+        # version 2.3.1 has bug, with returning wrong exit
+        # code in some cases, in newer version mcollective
+        # it was fixed
+        # https://github.com/puppetlabs/marionette-collective
+        #        /commit/10f163550bc6395f1594dacb9f15a86d4a3fde27
+        # So, it's just fixed code from Shell#runcommand
+        thread = Thread.current
+        stdout = ''
+        stderr = ''
+        status = systemu(cmd, {'stdout' => stdout, 'stderr' => stderr}) do |cid|
+          begin
+            while(thread.alive?)
+              sleep 0.1
+            end
+            Process.waitpid(cid) if Process.getpgid(cid)
+          rescue SystemExit
+          rescue Errno::ESRCH
+          rescue Errno::ECHILD
+          rescue Exception => e
+            Log.info("Unexpected exception received while waiting for child process: #{e.class}: #{e}")
+          end
+        end
+
+        [stdout, stderr, status.exitstatus]
       end
 
     end
