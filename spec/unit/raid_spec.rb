7a20c27d01a22dd515d3c18fcfb7fcbc01ab29da
Ivan Ponomarev <ivanzipfer@gmail.com>
2014-06-04 12:38:12 +0400

    add avago raid support
      -add main module raid
      -add avago raid support
      -add unit tests
    
    1) This module realized the support raid configuration
    and can be extended to the other raid vendors
    2) Currently includes support for Avago raid
    
    Change-Id: If7dafee57168814e6e12779c2cb989f5e661bb67

---
+++ spec/unit/raid_spec.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__), '../spec_helper')
+
+describe Astute::Raid do
+  include SpecHelpers
+
+  let(:ctx) do
+    ctx = mock('context')
+    ctx.stubs(:task_id)
+    ctx.stubs(:reporter)
+    ctx.stubs(:status).returns(1 => 'success', 2 => 'success')
+    ctx
+  end
+
+  let(:raid){Astute::Raid.new(ctx, [{node: 1}], 'lsi', 'cli', [])}
+
+  #context 'api' do
+  #  let(:interface) {'api'}
+  #end
+
+  it 'should call lsi' do
+    raid.expects(:lsi).once
+    raid.exec
+  end
+end
