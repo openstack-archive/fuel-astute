2c2c8da42f679b07f56138717e8b099a9dfa812d
Vladimir Sharshov <vsharshov@mirantis.com>
2014-07-07 16:03:21 +0400

    Sync time between cluster nodes before deployment
    This fix prevent sutuations like this: nova-network
    in XXX state on all of computes because date was different
    from master-node
    
    Change-Id: I980d10d56addf32d56ac49a600ab9fc7ea5831aa
    Closes-Bug: #1297293

--- spec/unit/nailyfact_deploy_spec.rb
+++ spec/unit/nailyfact_deploy_spec.rb
@@ -52,6 +52,7 @@ describe "NailyFact DeploymentEngine" do
       deploy_engine.stubs(:upload_ssh_keys).with(uniq_nodes_uid, deploy_data.first['deployment_id'])
       deploy_engine.stubs(:sync_puppet_manifests).with(deploy_data.uniq { |n| n['uid'] })
       deploy_engine.stubs(:enable_puppet_deploy).with(uniq_nodes_uid)
+      deploy_engine.stubs(:sync_time)
     end
 
     context 'log parsing' do
