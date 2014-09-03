966d4698693d3727bf873c5f5cf2905ee7808506
Bogdan Dobrelya <bdobrelia@mirantis.com>
2014-09-03 18:01:08 +0300

    Add stop all OST services pre-deploy hook
    For given (hook hardcode) services,
    * 1st try to stop specified services
      gracefully with stop action
    * 2nd, if there are some services' related processes
      are still running, send SIGKILL for all their childs as well.
    * Add unit test with hardcode services list
    
    Related-bug: #1364068
    
    Change-Id: Ida1eb0fbedbd93c2898142205a09c6ee351eeac4
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

---
+++ lib/astute/pre_node_actions/stop_ost_services.rb
@@ -0,0 +1,53 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+module Astute
+  class StopOSTServices < PreNodeAction
+
+    def process(deployment_info, context)
+      old_env = deployment_info.first['openstack_version_prev']
+      return if old_env.nil?
+
+      Astute.logger.info "Stop all Openstack services hook start"
+
+      target_file = '/tmp/stop_services.rb'
+      source_file = 'stop_services.script'
+      file_content = File.read File.join(File.dirname(__FILE__), source_file)
+      node_uids = deployment_info.collect { |n| n['uid'] }
+
+      Astute.logger.info "Uploading file: #{target_file} to nodes uids: #{node_uids.join ', '}"
+
+      MClient.new(context, "uploadfile", node_uids).upload(
+        :path => target_file,
+        :content => file_content,
+        :user_owner => 'root',
+        :group_owner => 'root',
+        :permissions => '0700',
+        :overwrite => true,
+        :parents => true
+      )
+
+      Astute.logger.info "Running file: #{target_file} on node uids:  #{node_uids.join ', '}"
+
+      response = run_shell_command(context, node_uids, "/usr/bin/ruby #{target_file} |tee /tmp/stop_services.log")
+
+      if response[:data][:exit_code] != 0
+        Astute.logger.warn "#{context.task_id}: Script returned error code #{response[:data][:exit_code]}"
+      end
+
+      Astute.logger.info "#{context.task_id}: Finished stop services pre-patching hook"
+    end #process
+
+  end #class
+end #module
