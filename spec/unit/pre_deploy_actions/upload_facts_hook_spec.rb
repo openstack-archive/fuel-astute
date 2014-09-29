61db181f0c7cc484ddf07aecdaff1ae6887bd336
Vladimir Sharshov <vsharshov@mirantis.com>
2014-09-29 16:28:50 +0400

    Upload facts for each role separately
    Instead of force rewriting of astute.yaml
    upload role facts to /etc/<role>.yaml and
    then do symbolic to astute.yaml
    
    Also:
    - add tests;
    - update logs for log parsing tests.
    
    Change-Id: Ie66b59f4dfa99c5038cab17c1de57e975c62a986

---
+++ spec/unit/pre_deploy_actions/upload_facts_hook_spec.rb
@@ -0,0 +1,74 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+require File.join(File.dirname(__FILE__), '../../spec_helper')
+
+describe Astute::UploadFacts do
+  include SpecHelpers
+
+  let(:ctx) do
+    ctx = mock('context')
+    ctx.stubs(:task_id)
+    ctx.stubs(:reporter)
+    ctx.stubs(:status).returns('1' => 'success', '2' => 'success')
+    ctx
+  end
+
+  let(:deploy_data) { [
+                        {'uid' => '1',
+                         'role' => 'controller',
+                         'openstack_version_prev' => 'old_version',
+                         'cobbler' => {
+                            'profile' => 'centos-x86_64'
+                          }
+                        }
+                      ]
+                    }
+
+  let(:upload_facts) { Astute::UploadFacts.new }
+
+  let(:mclient) do
+    mclient = mock_rpcclient(deploy_data)
+    Astute::MClient.any_instance.stubs(:rpcclient).returns(mclient)
+    Astute::MClient.any_instance.stubs(:log_result).returns(mclient)
+    Astute::MClient.any_instance.stubs(:check_results_with_retries).returns(mclient)
+    mclient
+  end
+
+  it 'should upload facts using YAML format to nodes in <role>.yaml file' do
+    mclient.expects(:upload).with(
+      :path =>'/etc/controller.yaml',
+      :content => deploy_data.first.to_yaml,
+      :overwrite => true,
+      :parents => true,
+      :permissions => '0600'
+    )
+
+    upload_facts.stubs(:run_shell_command).once.returns(:data => {:exit_code => 0})
+    upload_facts.process(deploy_data, ctx)
+  end
+
+  it 'should symbolic <role>.yaml file to astute.yaml one' do
+    mclient.stubs(:upload)
+
+    upload_facts.expects(:run_shell_command).with(
+      ctx,
+      ['1'],
+      'ln -s -f /etc/controller.yaml /etc/astute.yaml'
+    ).returns(:data => {:exit_code => 0})
+
+    upload_facts.process(deploy_data, ctx)
+  end
+
+end
