113398c9314d8be91c36cf295373cc2c37c5221e
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2014-11-17 16:01:14 +0300

    Add retries in case of Cobbler overloading
    Offen if Cobbler run two big task in parallel
    we got error based on Cobbler overloading.
    If we wait and try request again, it will
    process without problem. This change
    implement such behavior.
    
    Change-Id: I1e9774b9398dfb502159d7ba21e7e5b55b75b3a1
    Closes-Bug: #1387699

--- lib/astute/cobbler.rb
+++ lib/astute/cobbler.rb
@@ -106,6 +106,14 @@ module Astute
 
       def sync
         remote.call('sync', token)
+      rescue Net::ReadTimeout => e
+        retries ||= 0
+        retries += 1
+        raise e if retries > 2
+
+        Astute.logger.warn("Cobbler problem. Try to repeat: #{retries} attempt")
+        sleep 10
+        retry
       end
 
       def power(name, action)
