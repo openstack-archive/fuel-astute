2accfede50e4226ac67eed793caea057d74df4e2
Łukasz Oleś <loles@mirantis.com>
2014-12-03 16:27:11 +0100

    Speedup network verification
    Instead of sending test packets from each node one by one, sending them from group of nodes.
    Number of nodes may be increased in future.
    
    Closes-Bug: #1378500
    Change-Id: Ia970692de1778b15d2401b4758d47bc1343c02b7
    Blueprint: 200-nodes-support

--- spec/unit/network_spec.rb
+++ spec/unit/network_spec.rb
@@ -67,20 +67,21 @@ describe Astute::Network do
       rpcclient = mock_rpcclient(nodes)
 
       rpcclient.expects(:get_probing_info).once.returns([mc_res1, mc_res2])
-      nodes.each do |node|
-        rpcclient.expects(:discover).with(:nodes => [node['uid']]).at_least_once
+      uids = nodes.map { |node| node['uid'].to_s }
 
-        data_to_send = {}
-        node['networks'].each{ |net| data_to_send[net['iface']] = net['vlans'].join(",") }
 
-        rpcclient.expects(:start_frame_listeners).
-          with(:interfaces => data_to_send.to_json).
-          returns([mc_valid_res]*2)
+      data_to_send = {}
+      nodes[0]['networks'].each{ |net| data_to_send[net['iface']] = net['vlans'].join(",") }
+      data_to_send = { '1' => data_to_send, '2' => data_to_send}
 
-        rpcclient.expects(:send_probing_frames).
-          with(:interfaces => data_to_send.to_json).
-          returns([mc_valid_res]*2)
-      end
+      rpcclient.expects(:start_frame_listeners).
+        with(:interfaces => data_to_send.to_json).
+        returns([mc_valid_res]*2)
+
+      rpcclient.expects(:send_probing_frames).
+        with(:interfaces => data_to_send.to_json).
+        returns([mc_valid_res]*2)
+      rpcclient.expects(:discover).with(:nodes => uids)
       Astute::MClient.any_instance.stubs(:rpcclient).returns(rpcclient)
 
       res = Astute::Network.check_network(Astute::Context.new('task_uuid', reporter), nodes)
