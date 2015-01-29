af821cde536c872abac64b201eaf274599588c03
Kamil Sambor <ksambor@mirantis.com>
2015-01-29 10:15:01 +0100

    Migrate sync_time into granular deployment taks
    * modified shell_hook to take new params to make retries of commands
    * remove SyncTime from pre deployment actions for granular deployment
      because it is now implemented as taks
    
    implements blueprint granular-deployment-based-on-tasks
    
    Change-Id: I43da4b2382ad172fea27ca1abb7544a60fcd08cb

--- lib/astute/nailgun_hooks.rb
+++ lib/astute/nailgun_hooks.rb
@@ -103,21 +103,27 @@ module Astute
 
       timeout = hook['parameters']['timeout'] || 300
       cwd = hook['parameters']['cwd'] || "/"
+      retries = if hook['parameters']['retries'] then Astute.config.MC_RETRIES else 1
 
       shell_command = "cd #{cwd} && #{hook['parameters']['cmd']}"
 
-      is_success = true
+      is_success = false
+      
       perform_with_limit(hook['uids']) do |node_uids|
-        response = run_shell_command(
-          @ctx,
-          node_uids,
-          shell_command,
-          timeout,
-          cwd
-        )
-        if response[:data][:exit_code] != 0
-          Astute.logger.warn("Shell command failed. Check debug output for details")
-          is_success = false
+        retries.times do |retry_number|
+          response = run_shell_command(
+            @ctx,
+            node_uids,
+            shell_command,
+            timeout,
+            cwd
+          )
+          if response[:data][:exit_code] == 0
+            is_success = true
+            break
+          end
+          Astute.logger.warn("Problem while performing cmd. Try to repeat: #{retry_number} attempt")
+          sleep Astute.config.MC_RETRY_INTERVAL
         end
       end
 
