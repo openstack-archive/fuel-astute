7a493333a1f6d74fe481c02a52553bb4d2e6a5b8
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-03-02 13:44:47 +0300

    Added running pre_provision_tasks
    We need to build target images before we
    can use them for IBP. Nailgun serializes
    pre-provision tasks and puts them under
    pre_provision_tasks key in provisioning_info.
    
    Depends on: Ie404388cc3093a20477ea1a9771ef9e9645d17c3
    Implements: blueprint consume-external-ubuntu
    
    Change-Id: I88df9ea250a1541f53d9f6efe0be9f7e189ae97e

--- spec/unit/orchestrator_spec.rb
+++ spec/unit/orchestrator_spec.rb
@@ -81,7 +81,7 @@ describe Astute::Orchestrator do
         "username"=>"cobbler",
         "password"=>"cobbler",
         "master_ip"=>"127.0.0.1",
-        "provision_method"=>"cobbler",
+        "provision_method"=>"classic",
       },
       "task_uuid"=>"a5c44b9a-285a-4a0c-ae65-2ed6b3d250f4",
       "nodes" => [
