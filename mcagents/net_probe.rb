9022678842b28fa66d499f5ba348fe815944b4da
Dmitry Shulyak <dshulyak@mirantis.com>
2015-05-15 18:39:35 +0300

    [WIP] Add repo availability verification with network setup
    Change-Id: I3216b97df5e7b8974ac7155d06446c4419d293a1

--- mcagents/net_probe.rb
+++ mcagents/net_probe.rb
@@ -72,7 +72,23 @@ module MCollective
 
         cmd = "urlaccesscheck check '#{urls.join("' '")}'"
         reply[:status] = run(cmd, :stdout => :out, :stderr => :err)
+        reply[:stdout] = JSON.parse(reply[:stdout])
       end
+
+      action "check_repositories_with_setup" do
+        config = request.data[:data][get_uid]
+
+        cmd = "urlaccesscheck with setup \
+          -i #{config['iface']} \
+          -g #{config['gateway']} \
+          -a #{config['addr']} \
+          -v #{config['vlan']} \
+          '#{config['urls'].join("' '")}'"
+
+        reply[:status] = run(cmd, :stdout => :out, :stderr => :err)
+        reply[:stdout] = JSON.parse(reply[:stdout])
+      end
+
 
       private
 
