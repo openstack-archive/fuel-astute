34e0493afa22999c4a07d3198ceb945116ab7932
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2015-07-22 20:09:26 +0300

    Support Mac OS X to run tests
    Change-Id: If67bece4675f5d2cb34b70a176a88dfaad0c23c4
    Closes-Bug: #1450575

--- run_tests.sh
+++ run_tests.sh
@@ -4,16 +4,15 @@
 
 set -e
 
-function cd_workspace() {
-  cd $(dirname $(readlink -f $0)) > /dev/null
-}
+ROOT_WORKSPACE=$(cd `dirname $0` && pwd -P)
 
 function license_check() {
   # License information must be in every source file
-  cd_workspace
+  cd $ROOT_WORKSPACE
 
-  tmpfile=`mktemp`
-  find * -not -path "docs/*" -regex ".*\.\(rb\)" -type f -print0 | xargs -0 grep -Li License
+
+  tmpfile=`mktemp -t _fuel-astute_.XXXXXXX`
+  find * -not -path "docs/*" -regex ".*\.\(rb\)" -type f -print0 | xargs -0 grep -Li License > $tmpfile
   files_with_no_license=`wc -l $tmpfile | awk '{print $1}'`
   if [ $files_with_no_license -gt 0 ]; then
     echo "ERROR: Found files without license, see files below:"
@@ -25,7 +24,7 @@ function license_check() {
 }
 
 function ruby_checks() {
-  cd_workspace
+  cd $ROOT_WORKSPACE
 
   # Install all ruby dependencies (expect ruby version manager: RVM, rbenv or similar)
   bundle install
