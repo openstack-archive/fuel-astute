7a2c385bc1cc2e745fdc0d7c35679bc2d00a1b24
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2015-08-27 20:08:15 +0300

    Implement asynchronous logger for event callbacks
    Excludes any lock operations in EventMachine event loop
    and its callbacks.
    
    Now in EventMachine callbacks all writings to log are
    performed in asynchronous fashion. This way we try to
    exclude possibility of blocking and getting deadlock
    using classic logging mechanism, which has plenty of
    locks internally.
    
    Also similar problem was seen in the next ticket #1453573.
    
    Co-Authored-By: Evgeniy L <eli@mirantis.com>
    
    Closes-Bug: #1487397
    Change-Id: I72d0eb01ef8c87e10003338bd3bb70e42a4b7dd6

---
+++ lib/astute/server/async_logger.rb
@@ -0,0 +1,85 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+require 'thread'
+
+module Astute
+  module Server
+
+    # Asynchronous singleton logger, which should be used
+    # in event callbacks of event machine, it doesn't block
+    # callbacks because writing a message to log takes some time.
+    # Also synchronous logger, potentially could lead to deadlocks.
+    # See:
+    # https://bugs.launchpad.net/fuel/+bug/1453573
+    # https://bugs.launchpad.net/fuel/+bug/1487397
+    module AsyncLogger
+      def self.start_up(logger=Logger.new(STDOUT))
+        @queue ||= Queue.new
+        @shutdown = false
+        @log = logger
+        @thread = Thread.new { wrtie_message }
+      end
+
+      def self.shutdown
+        # Shutdown logger gracefully, wait until all messages
+        # are written into the log
+        @shutdown = true
+        @thread.join
+      end
+
+      def self.add(severity, msg=nil)
+        return if @shutdown
+
+        @queue.push([severity, msg])
+      end
+
+      def self.debug(msg=nil)
+        add(Logger::Severity::DEBUG, msg)
+      end
+
+      def self.info(msg=nil)
+        add(Logger::Severity::INFO, msg)
+      end
+
+      def self.warn(msg=nil)
+        add(Logger::Severity::WARN, msg)
+      end
+
+      def self.error(msg=nil)
+        add(Logger::Severity::ERROR, msg)
+      end
+
+      def self.fatal(msg=nil)
+        add(Logger::Severity::FATAL, msg)
+      end
+
+      def self.unknown(msg=nil)
+        add(Logger::Severity::UNKNOWN, msg)
+      end
+
+    private
+
+      def self.wrtie_message
+        until @shutdown do
+          until @queue.empty?
+            severity, msg = @queue.pop
+            @log.add(severity, msg)
+          end
+        end
+      end
+
+    end
+  end
+end
