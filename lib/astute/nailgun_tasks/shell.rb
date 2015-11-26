c2ff936e1469d71eb7cdc4ad817621e7d56875e0
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2015-11-26 18:17:29 +0300

    Base task deployment
    * all tasks now async, including shell;
    * new report format based on task progress instead of log;
    * support union graph deployment: pre/post tasks included in main graph;
    * plugins tasks also included in main graph;
    * support previous failt tollerance strategy (percent of failed nodes).
    
    Closes-Bug: #1506962
    Closes-Bug: #1430470
    implements: task-based-deployment-astute
    
    Change-Id: Ibe24d047ab502b88ea0ae9f8c77f94440000d289

---
+++ lib/astute/nailgun_tasks/shell.rb
@@ -0,0 +1,124 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+require 'erb'
+
+module Astute
+  class Shell < NailgunTask
+
+    private
+
+    SHELL_MANIFEST_DIR = '/etc/puppet/shell_manifests'
+    PUPPET_MODULES_DIR = '/etc/puppet/modules'
+
+    def process
+      upload_shell_manifest
+      @puppet_task = Puppet.new(
+        generate_puppet_hook,
+        @ctx
+      ).run
+    end
+
+    def calculate_status
+      status = @puppet_task.status
+    end
+
+    def pre_validation
+      validate_presence(@hook, 'uids')
+      validate_presence(@hook['parameters'], 'cmd')
+    end
+
+    def setup_default
+      @hook['parameters']['timeout'] ||= 300
+      @hook['parameters']['cwd'] ||= "/"
+      @hook['parameters']['retries'] ||= Astute.config.mc_retries
+      @hook['parameters']['interval'] ||= Astute.config.mc_retry_interval
+    end
+
+    def puppet_exec_template
+      template = <<-eos
+    # Puppet manifest wrapper for task: <%= task_name %>
+    notice('MODULAR: <%= task_name %>')
+
+    exec { '<%= task_name %>_shell' :
+      path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      command   => '/bin/bash "<%= shell_exec_file_path %>"',
+      logoutput => true,
+    }
+      eos
+      ERB.new(template, nil, '-').result(binding)
+    end
+
+    def shell_exec_template
+      command = "cd #{@hook['parameters']['cwd']} &&" \
+                " #{@hook['parameters']['cmd']}"
+      template = <<-eos
+    #!/bin/bash
+    # Puppet shell wrapper for task: <%= task_name %>
+    # Manifest: <%= puppet_exec_file_path task_name %>
+
+    <%= command %>
+      eos
+      ERB.new(template, nil, '-').result(binding)
+    end
+
+    def shell_exec_file_path
+      File.join(SHELL_MANIFEST_DIR, "#{task_name}_command.sh")
+    end
+
+    def puppet_exec_file_path
+      File.join(SHELL_MANIFEST_DIR, manifest_name)
+    end
+
+    def upload_puppet_manifest
+      upload_file(@hook['uids'], {
+        :path => puppet_exec_file_path,
+        :content => puppet_exec_template,
+        :permissions => '0755'
+      })
+    end
+
+    def upload_shell_file
+      upload_file(@hook['uids'], {
+        :path => shell_exec_file_path,
+        :content => shell_exec_template,
+        :permissions => '0755'
+      })
+    end
+
+    def upload_shell_manifest
+      upload_puppet_manifest
+      upload_shell_file
+    end
+
+
+    def manifest_name
+      "#{task_name}_manifest.pp"
+    end
+
+    def generate_puppet_hook
+      {
+        'uids' => @hook['uids'],
+        'id' => @hook['id'] + '_puppet',
+        'parameters' =>  {
+          "puppet_manifest" =>  manifest_name,
+          "puppet_modules" =>  PUPPET_MODULES_DIR,
+          "cwd" => SHELL_MANIFEST_DIR,
+          "timeout" =>  @hook['parameters']['timeout'],
+          "retries" => @hook['parameters']['retries']
+        }
+      }
+    end
+
+  end
+end
