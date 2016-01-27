acc9b0322d7ee010b30b754a3ee580633b142224
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2016-01-27 14:35:54 +0300

    Gracefully stop for task based deployment
    Instead of classic stop deployment now we stop to
    process new tasks, but allow to end already running tasks.
    
    It will give us ability to run tasks again without erasing
    nodes.
    
    Also enable task based logging
    
    Change-Id: I23b9222ac86522c0879e5ab3c7554e554df65fcc
    Implements: blueprint stop-resume-stop-deployment

---
+++ lib/astute/task_cluster.rb
@@ -0,0 +1,39 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+require 'fuel_deployment'
+
+module Astute
+  class TaskCluster < Deployment::Cluster
+    attr_accessor :gracefully_stop_mark
+
+    def stop_condition(&block)
+      self.gracefully_stop_mark = block
+    end
+
+    def post_node_poll(*args)
+      gracefully_stop(args[0])
+    end
+
+    # Check if the deployment process should stop
+    # @return [true, false]
+    def gracefully_stop?
+      gracefully_stop_mark ? gracefully_stop_mark.call : false
+    end
+
+    def gracefully_stop(node)
+      node.set_status_skipped if node.ready? && gracefully_stop?
+    end
+
+  end
+end
