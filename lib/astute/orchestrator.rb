d10594c00172a9ab31e9663bdce1403265322bdd
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2016-02-03 18:04:23 +0300

    Exclude master node availability check
    We need to run repository availability check not only
    for clusternode but for master node too. Current
    availability check use systemtype mcollective agent
    which check special node file which is missing on master node.
    So we need to exclude master node availability check.
    
    Change-Id: I76ed1f0b6413cc94c539d795bbba92f8e9dad50b
    Partial-Bug: #1540346

--- lib/astute/orchestrator.rb
+++ lib/astute/orchestrator.rb
@@ -227,8 +227,11 @@ module Astute
     end
 
     def validate_nodes_access(ctx, nodes)
-      nodes_types = node_type(ctx.reporter, ctx.task_id, nodes.map{ |n| n['uid'] }, timeout=10)
-      not_avaliable_nodes = nodes.map { |n| n['uid'].to_s } - nodes_types.map { |n| n['uid'].to_s }
+      # Do not check master node availability, because it does not contain
+      # node type file
+      node_ids = nodes.map{ |n| n['uid'].to_s } - ['master']
+      nodes_types = node_type(ctx.reporter, ctx.task_id, node_ids, timeout=10)
+      not_avaliable_nodes = node_ids - nodes_types.map { |n| n['uid'].to_s }
       unless not_avaliable_nodes.empty?
         raise "Network verification not avaliable because nodes #{not_avaliable_nodes} " \
           "not avaliable via mcollective"
