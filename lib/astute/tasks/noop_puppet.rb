df4f187c0b742aa1073872482288bd111e6878af
Denis Egorenko <degorenko@mirantis.com>
2016-08-04 19:30:23 +0300

    Add ability to run puppet with noop
    This patch adds ability to run deployment graph with noop option.
    In same time, this option will be applied only to 'shell' and
    'puppet' types only.
    
    Change-Id: Ibcb275bb84dfd553ab07e6d58af753ecf96ab3a5

---
+++ lib/astute/tasks/noop_puppet.rb
@@ -0,0 +1,52 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+require 'astute/tasks/puppet'
+
+module Astute
+  class NoopPuppet < Puppet
+
+    REPORT_FILE = Astute.config.puppet_noop_report + task_name + '.json'
+
+    def summary
+      File.open(REPORT_FILE).readlines
+    rescue
+      {}
+    end
+
+    private
+
+    def create_puppet_task
+      PuppetTask.new(
+        Context.new(
+          @ctx.task_id,
+          PuppetLoggerReporter.new,
+          LogParser::NoParsing.new
+        ),
+        {'uid' => @task['node_id'].to_s, 'role' => task_name},
+        {
+          :retries => @task['parameters']['retries'],
+          :puppet_manifest => @task['parameters']['puppet_manifest'],
+          :puppet_modules => @task['parameters']['puppet_modules'],
+          :cwd => @task['parameters']['cwd'],
+          :timeout => @task['parameters']['timeout'],
+          :puppet_debug => @task['parameters']['debug'],
+          :puppet_noop => true,
+          :puppet_noop_report => REPORT_FILE
+        }
+      )
+    end
+
+  end
+end
